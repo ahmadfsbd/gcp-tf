0247dc5a5d99fb8a2cf1c4b3f2caf5eddf56bf44
Muhammad Ahmad <ahmadfsbd@gmail.com>
2025-03-11 14:03:10 +0000

    define providers and module dirs

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,16 +1,13 @@
 variable "project" {
     type = string
-    default = "teraform101"
 }
 
 variable "region" {
     type = string
-    default = "europe-west2"
 }
 
 variable "zone" {
     type = string
-    default = "europe-west2-a"
 }
 
 #eof
